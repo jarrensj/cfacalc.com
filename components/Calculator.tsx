2bed547b37d310ea81bbf4801f7d386e1d9c802b
Jarren San Jose <sanjose.jarren@gmail.com>
2024-02-03 15:52:42 -0800

    add loading state

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -11,10 +11,13 @@ type Item = {
 
 const Calculator: React.FC = () => {
   const [data, setData] = useState<Item[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true); 
+
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true); 
         const response = await fetch('/api/data');
         const jsonData: Item[] = await response.json();
         const itemsWithValue = jsonData.map(item => ({
@@ -26,6 +29,8 @@ const Calculator: React.FC = () => {
         setData(itemsWithValue);
       } catch (error) {
         console.error("Failed to fetch data", error);
+      } finally {
+        setIsLoading(false); 
       }
     };
 
@@ -35,18 +40,22 @@ const Calculator: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-2xl font-bold my-4">Optimal Points Redemption</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data.map((item, index) => (
-          <div key={index} className="border p-4 rounded-lg shadow">
-            <h2 className="text-lg font-semibold">{item.item}</h2>
-            <p>Price: ${item.price.toFixed(2)}</p>
-            <p>Points: {item.points}</p>
-            {item.valuePerPoint && (
-                <p>Value per point: ${item.valuePerPoint.toFixed(7)}</p>
-            )}
-          </div>
-        ))}
-      </div>
+      {isLoading ? (
+        <div>Loading...</div> 
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {data.map((item, index) => (
+            <div key={index} className="border p-4 rounded-lg shadow">
+              <h2 className="text-lg font-semibold">{item.item}</h2>
+              <p>Price: ${item.price.toFixed(2)}</p>
+              <p>Points: {item.points}</p>
+              {item.valuePerPoint && (
+                  <p>Value per point: ${item.valuePerPoint.toFixed(7)}</p>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
