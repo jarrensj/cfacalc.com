6f7b37956623164ed6c29c046938451e36e5a278
Jarren San Jose <sanjose.jarren@gmail.com>
2024-02-03 12:24:31 -0800

    cfa calc

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-8">hi</h1>
+      <h1 className="text-4xl font-bold mb-8">cfa calc</h1>
     </main>
   );
 }
