a5569cf2f1297d03964cc5f06c5b059d0c460162
Jarren San Jose <sanjose.jarren@gmail.com>
2024-02-03 12:34:06 -0800

    add description about the app

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold mb-8">cfa calc</h1>
+      <p className="text-xl mb-8">
+        A calculator to see what&apos;s the most optimal way to spend your points.
+      </p>
     </main>
   );
 }
